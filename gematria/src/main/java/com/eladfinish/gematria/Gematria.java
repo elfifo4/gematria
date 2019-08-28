ebaab58fce31916d531a8392e2dbaa8637a89c87
elfifo4 <elfifo4@gmail.com>
2019-08-28 21:42:31 +0300

    Added method gematriaWord
    gets a word and return its gematria value

--- gematria/src/main/java/com/eladfinish/gematria/Gematria.java
+++ gematria/src/main/java/com/eladfinish/gematria/Gematria.java
@@ -106,6 +106,96 @@ public class Gematria {
     }
 
 
+    public static int gematriaWord(String word) {
+        int sum = 0;
+
+        for (int i = 0; i < word.length(); i++)
+            sum += gematriaOt(word.charAt(i));
+
+        return sum;
+    }
+
+    private static int gematriaOt(char ot) {
+        int num;
+        switch (ot) {
+            case 'א':
+                num = 1;
+                break;
+            case 'ב':
+                num = 2;
+                break;
+            case 'ג':
+                num = 3;
+                break;
+            case 'ד':
+                num = 4;
+                break;
+            case 'ה':
+                num = 5;
+                break;
+            case 'ו':
+                num = 6;
+                break;
+            case 'ז':
+                num = 7;
+                break;
+            case 'ח':
+                num = 8;
+                break;
+            case 'ט':
+                num = 9;
+                break;
+            case 'י':
+                num = 10;
+                break;
+            case 'כ':
+            case 'ך':
+                num = 20;
+                break;
+            case 'ל':
+                num = 30;
+                break;
+            case 'מ':
+            case 'ם':
+                num = 40;
+                break;
+            case 'נ':
+            case 'ן':
+                num = 50;
+                break;
+            case 'ס':
+                num = 60;
+                break;
+            case 'ע':
+                num = 70;
+                break;
+            case 'פ':
+            case 'ף':
+                num = 80;
+                break;
+            case 'צ':
+            case 'ץ':
+                num = 90;
+                break;
+            case 'ק':
+                num = 100;
+                break;
+            case 'ר':
+                num = 200;
+                break;
+            case 'ש':
+                num = 300;
+                break;
+            case 'ת':
+                num = 400;
+                break;
+            default:
+                num = 0;
+        }
+        return num;
+    }
+    
+    
 //    public static void main(String[] args) {
 //        System.out.println(getOtiot(123));
 //    }
